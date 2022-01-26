d36ba3e8dc554d78b919f4ed29ff156da66a2526
ypack <ypack@mingyuanyun.com>
2022-01-26 10:04:58 +0800

    [add]修改

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <div id="yourContainer"></div>
+    <!-- <router-view /> -->
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { registerMicroApps, start, loadMicroApp, prefetchApps } from "qiankun";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  mounted() {
+    registerMicroApps([
+      {
+        name: "react app", // app name registered
+        entry: "//localhost:8082",
+        container: "#yourContainer",
+        activeRule: "#/about",
+      },
+      // {
+      //   name: "vue app",
+      //   entry: { scripts: ["//localhost:7100/main.js"] },
+      //   container: "#yourContainer2",
+      //   activeRule: "/yourActiveRule2",
+      // },
+    ]);
 
+    prefetchApps([{
+        name: "react app", // app name registered
+        entry: "//localhost:8082",
+        container: "#yourContainer",
+        activeRule: "#/about",
+    }])
+    //     const MicroApp = loadMicroApp({
+    //     name: "react app", // app name registered
+    //     entry: "//localhost:8082",
+    //     container: "#yourContainer",
+    //     activeRule: "#/about",
+    // })
+    // console.log(MicroApp)
+    // MicroApp.bootstrapPromise();
+    start();
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -23,6 +53,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+
+
+import "whatwg-fetch";
+import "custom-event-polyfill";
+import "core-js/stable/promise";
+import "core-js/stable/symbol";
+import "core-js/stable/string/starts-with";
+import "core-js/web/url";
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
